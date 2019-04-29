047342989302e4ea8fc851f952e988f1c5e9a5df
Kaio Cesar <tecnico.kaio@gmail.com>
2019-04-29 12:01:26 -0300

    card_005 init cliente class

--- cliente.py
+++ cliente.py
@@ -2,4 +2,12 @@
 
 
 class Cliente(object):
-    pass
+
+    def __init__(
+        self,
+        nome: str = None,
+        sobrenome: str = None,
+        cpf: str = None):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
